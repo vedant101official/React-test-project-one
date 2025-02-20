e65f7eaa51aa960b06c67754e1bd864db1cc8d84
vedant101official <vedant101official@gmail.com>
2025-02-21 04:24:32 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "hello world from react "
+);
+
+const parents = React.createElement(
+    "div",
+    { id: "parents" },
+    React.createElement(
+        "div",
+        { id: "child" },
+        [React.createElement(
+            "h1",
+            {},
+            "hello world from react h1"
+        ),
+        React.createElement(
+            "h2",
+            {},
+            "hello world from react h2"
+        )]
+
+    )
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parents);
